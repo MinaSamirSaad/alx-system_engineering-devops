12f6aa0d7d4d8b60c5d0e62d7eae480b3b9ed6d2
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-19 16:40:45 +0200

    Install flask using puppet package configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,7 @@
 # Install flask using pip3
 # ensure it is of version 2.1.0
 
-package { 'python3-pip':
-    ensure  => present
-}
-
 package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip']
 }
