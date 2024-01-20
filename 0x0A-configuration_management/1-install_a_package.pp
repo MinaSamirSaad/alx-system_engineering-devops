3a02df379f897b7e2e8aee8b7dd5ca4b96c4274d
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-01-20 20:24:49 +0200

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,18 @@
 # Install flask using pip3
 # ensure it is of version 2.1.0
 
-package { 'flask':
+package {'python3-pip' :
+    ensure => installed,
+}
+
+package { 'flask' :
     ensure   => '2.1.0',
     provider => 'pip3',
+    require  => Package['python3-pip']
+}
+
+package { 'werkzeug' :
+    ensure   => '2.1.1',
+    provider => 'pip3',
+    require  => Package['python3-pip']
 }
